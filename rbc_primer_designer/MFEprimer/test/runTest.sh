ca14d8dc85829aebfb79a9d031c36e3c4583fe62
warthmann <norman@warthmann.com>
2020-04-14 14:22:48 +0200

    compressed old MFE_primer

--- rbc_primer_designer/MFEprimer/test/runTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#../MFEprimer.py -i primers.fa -d ./database.seq -T F -e 10000 -W 11 -a 2 -s 0.2
-#../MFEprimer.py -i primers.fa -d ./database.seq -T F -e 10000 -W 11 -a 2 --ppc_cutoff=0.2 --seq=primer.mfe.fa -o primer.mfe
-../MFEprimer.py -i primers.fa -d ./database.seq -T F -e 10000 -W 4 -a 2 --ppc_cutoff=0.2 --seq=primer.mfe.fa -o primer.mfe
